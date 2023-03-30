614345e8f5a8056735d4f5c29f34c06e8cf1a2ce
SCP-939-83 <512707@vistacollege.nl>
2023-03-30 09:34:09 +0200

    e

--- front-end/Universal_header.js
+++ front-end/Universal_header.js
@@ -1,5 +1,23 @@
-function togglePopup() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+const openPopupBtn = document.getElementById('open-popup');
+const closePopupBtns = document.querySelectorAll('.popup button');
+const popup = document.getElementById('popup');
+
+openPopupBtn.addEventListener('click', function() {
+  popup.style.display = 'block';
+});
+
+closePopupBtns.forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    if (btn.id === 'yes-btn') {
+      // Do something if user clicked 'Yes'
+    }
+    popup.style.display = 'none';
+  });
+});
+
+// Close popup when clicking outside the popup box
+window.addEventListener('click', function(event) {
+  if (event.target === popup) {
+    popup.style.display = 'none';
   }
-  
+});
